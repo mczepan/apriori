b175644debb54de5d44dc75d217da78c9d1ac261
MariuszMackowski <m.mackowski@op.pl>
2020-01-09 09:40:33 +0100

    binaryzacja i wsparcie kilu zbiorow

--- apriori.py
+++ apriori.py
@@ -5,22 +5,39 @@ Created on Thu Jan  9 08:37:05 2020
 @author: Mario
 """
 
-def supp(A,B,*args):
-    return sum(A,B)
+def binaryzacja(X):
+    for i in range(0,len(X)):
+        for j in range(0,len(X[i])):
+            if(X[i][j] > 0):
+                X[i][j] = 1
+    return X
+
+def supp_laczone(*args):
+    s = 0
+    for i in range(len(args[0])):
+        row = []
+        for arg in args:
+            row.append(arg[i])        
+        if all(list(map(lambda x: x == 1,row))):
+           s+=1
+    return s
 
 
 def supp(A):
-    return sum(A)
+    if (type(A[0]) != list):
+        return sum(A)
+    else:
+        s = []
+        for a in A:
+            s.append(sum(a))
+        return s
 
 def zbiory_czeste(A,minSupp):
     for i in range(len(A)-1,-1,-1):
         print('A'+str(i+1),A[i])
         if supp(A[i])>minSupp:
             print('asda')
-#        j = i
-#        while j>0:
-#            print('A',str(j),":",supp(A[j-1]))
-#            j-=1
+
 
 
 A1 = [0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0]
@@ -33,6 +50,7 @@ A = [A1,A2,A3,A4]
 #print(supp(A))
 zbiory_czeste(A,6)
 
-print(A)
-print(supp(A))
+#print(A)
+#print(supp(A))
+print(supp_laczone(A3,A4))
 
