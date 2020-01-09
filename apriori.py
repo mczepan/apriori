6cdb935a9b706d2f2cf6a2655fc347ecabc153b3
MariuszMackowski <m.mackowski@op.pl>
2020-01-09 09:25:14 +0100

    asdas

--- apriori.py
+++ apriori.py
@@ -5,16 +5,29 @@ Created on Thu Jan  9 08:37:05 2020
 @author: Mario
 """
 
+def supp(A,B,*args):
+    return sum(A,B)
+
+
 def supp(A):
-    s = []
-    for zbior in A:
-        s.append(sum(zbior))
-    return s
+    return sum(A)
+
+def zbiory_czeste(A,minSupp):
+    for i in range(len(A)-1,-1,-1):
+        print('A'+str(i+1),A[i])
+        if supp(A[i])>minSupp:
+            print('asda')
+#        j = i
+#        while j>0:
+#            print('A',str(j),":",supp(A[j-1]))
+#            j-=1
+
 
 A1 = [0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0]
 A2 = [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0]
 A3 = [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1]
 A4 = [1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1]
 A = [A1,A2,A3,A4]
-print(A)
-print(supp(A))
+#print(A)
+#print(supp(A))
+zbiory_czeste(A,6)
